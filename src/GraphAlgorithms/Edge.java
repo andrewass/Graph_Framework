b2db228f6c159e3394610d6c599f5789d3e20a5e
andrewas <andrewas@student.matnat.uio.no>
2017-09-11 23:58:45 +0200

    added ConnectedComponents and MST

---
+++ src/GraphAlgorithms/Edge.java
@@ -0,0 +1,14 @@
+package GraphAlgorithms;
+/**
+ * Created by Andreas Wassum on 10.09.17.
+ */
+public class Edge {
+    public Vertex from,to;
+
+    public Edge(Vertex from, Vertex to){
+        this.from = from;
+        this.to = to;
+    }
+
+
+}
---
+++ src/GraphAlgorithms/Vertex.java
@@ -0,0 +1,19 @@
+package GraphAlgorithms;
+
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+
+
+/**
+ * Created by Andreas Wassum on 8/30/17.
+ */
+public class Vertex {
+    public int x,y;
+    public boolean marked, visited;
+    public Ellipse2D.Double ellipse;
+    public ArrayList<Edge> edges = new ArrayList<>();
+
+    public Vertex(int x, int y){        this.x = x;
+        this.y = y;
+    }
+}
